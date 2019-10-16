e20c74ca6b94fef77dc84176c0e31fb2bd693981
Armando Navarro <armando.navarro@iselect.com.au>
2019-10-16 15:05:12 +0800

    SC 82

--- page_objects/verticals/reusable/a_person_accountAndopportunity_page/sp_651_convert_lead.js
+++ page_objects/verticals/reusable/a_person_accountAndopportunity_page/sp_651_convert_lead.js
@@ -1,7 +1,36 @@
 module.exports = {
     url: 'https://iselect--bat.lightning.force.com/lightning/o/Lead/list?filterName=Recent',
     elements: {
-        searchField: 'input[placeholder=\"Search Salesforce\"]',
+        //Lead Creation
+        newLead: '[title="New"]',
+        newleadNext: 'button[class="slds-button slds-button--neutral slds-button slds-button_brand uiButton"]',
+        businessverticalDropdown: 'div.slds-gutters_small:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)',
+            broadband: '[title=\"Broadband\"]',
+            life: '[title=\"Life\"]',
+            generalinsurance: '[title=\"General Insurance\"]',
+            telco: '[title=\"Telco\"]',
+            energy: '[title=\"Energy\"]',
+            health: '[title=\"Health\"]',
+            hc: '[title=\"Home & Contents\"]',
+        salutationDropdown: '.salutation',
+        salutationMr: 'li.uiMenuItem:nth-child(4) > a:nth-child(1)',
+        firstName: 'input[class=\"firstName compoundBorderBottom form-element__row input\"]',
+        lastName: 'input[class=\"lastName compoundBorderBottom form-element__row input\"]',
+        email: 'input[type=\"email\"]',
+        leadsourceDropdown: 'div.slds-gutters_small:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)',
+        leadsourceIselect: 'li.uiMenuItem:nth-child(13) > a:nth-child(1)',
+        save: 'button[class=\"slds-button slds-button--neutral uiButton--brand uiButton forceActionButton\"]',
+        leadName: 'th[tabindex=\"-1\"]',
+        businessVertical: 'td[data-aura-rendered-by=\"3149:0\"]',
+        emailhome: 'td[data-aura-rendered-by=\"3175:0\"]',
+
+        //Lead List View
+        leadlistSearch: 'input[placeholder=\"Search this list...\"]',
+        matchedLead: lead.testdata.lead_selector,
+        leadsearchResult: 'table:nth-child(4) > th.slds-cell-edit:nth-child(2)',
+        recentLead: 'a:contains("Test Lead Conversion")',
+
+        searchField: 'input[placeholder=\"Search Leads and more\"]',
         convertLead: '[title=\"Convert Lead\"]',
         searchResult: 'ul[class=\"lookup__list  visible\"]',
         recentItems: '[role=\"option\"]',
@@ -34,49 +63,21 @@ module.exports = {
 
     commands: [{
 
-        click_changeNav() {
+        //Lead Creation
+        click_newLead() {
             return this
-            .click('@changeNav');
-        },
-        
-        click_viewMore() {
-            return this
-            .click('@viewMore');
+            .click('newLead');
         },
 
-        click_hyperlinkText() {
+        //Lead list search
+        click_leadlistSearch() {
             return this
-            .click('@hyperlinkText');
+            .click('leadlistSearch');
         },
 
-        click_expandList() {
+        click_recentLead() {
             return this
-            .click('@expandList');
-        },
-
-        click_reportsSec() {
-            return this
-            .click('@reportsSec');
-        },
-
-        click_searchResult() {
-            return this
-            .click('@searchResult');
-        },
-
-        click_accountSec() {
-            return this
-            .click('@accountSec');
-        },
-
-        click_leadsSec() {
-            return this
-            .click('@leadsSec');
-        },
-
-        click_opportunitiesSec() {
-            return this
-            .click('@opportunitiesSec');
+            .click('recentLead');
         },
 
         click_searchField() {
@@ -84,23 +85,74 @@ module.exports = {
             .click('@searchField');
         },
 
+        click_leadsearchResult() {
+            return this
+            .click('leadsearchResult');
+        },
+
+        click_matchedSearch() {
+            return this
+            .click('matchedSearch')
+        },
+
         click_convertLead() {
             return this
             .click('@convertLead');
         },
-        click_profiles() {
+
+        click_navDropdown() {
             return this
-            .click('@profilesDropdown');
+            .click('@navDropdown');
         },
 
-        click_tab0() {
+        click_leadDropdown() {
             return this
-            .click('@tab0');
+            .click('@leadDropdown');
         },
 
-        click_newAccount() {
+        click_newLead() {
             return this
-            .click('@newAccount');
+            .click('@newLead');
+        },
+        
+        click_next(){
+            return this
+            .click('@next');
+        },
+
+        click_salutationDropdown() {
+            return this
+            .click('@salutationDropdown');
+        },
+
+        click_salutationMr() {
+            return this
+            .click('@salutationMr');
+        },
+
+        click_leadsourceDropdown() {
+            return this
+            .click('@leadsourceDropdown');
+        },
+
+        click_leadsourceIselect() {
+            return this
+            .click('@leadsourceIselect');
+        },
+
+        click_save() {
+            return this
+            .click('@save');
+        },
+
+        click_businessverticalDropdown() {
+            return this
+            .click('@businessverticalDropdown');
+        },
+
+        click_energy() {
+            return this
+            .click('@energy');
         }
     }]
 };
--- tests/bat_env/verticals/energy/energysales_consultant/a_person_accountAndopportunity_tests/sp_651_convert_lead/ac1_convert_lead.js
+++ tests/bat_env/verticals/energy/energysales_consultant/a_person_accountAndopportunity_tests/sp_651_convert_lead/ac1_convert_lead.js
@@ -1,5 +1,5 @@
 module.exports = {
-    tags: ['bat_sp_651_ac1', 'bat_sp_651' , 'sp_651'],
+    tags: ['bat_energy_sp_651_ac1', 'bat_sp_651' , 'sp_651'],
 
     'Log in as Energy Consultant QA': async function(browser) 
     {
@@ -9,12 +9,49 @@ module.exports = {
     
     'GIVEN I have the potential lead in the Lead Page': function(browser) 
     {
-        
+        search
+            .waitForElementPresent('@searchField', 3000, function (result)
+            {
+                if (result.value)
+                {
+                    browser
+                        .url(lead.lead_list.url)
+                        .pause(1000);
+                    convert
+                        .useXpath().click('/html/body/div[5]/div[1]/div[2]/div[1]/div/div/div/div/div[2]/div/div[1]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[1]/th')
+                        .pause(2000);
+                }
+                    else
+                    {
+                        browser
+                            .url(lead.lead_list.url)
+                            .pause(1000);
+                        convert
+                            .useXpath().click('/html/body/div[5]/div[1]/div[2]/div[1]/div/div/div/div/div[2]/div/div[1]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[1]/th')
+                            .pause(2000);
+                    }
+
+            })  
     },
     
     'WHEN I click on the Convert Lead button': function (browser) 
     {
-        
+        convert
+            .waitForElementPresent('@convertLead', 3000, function (result)
+            {
+                if (result.value)
+                {
+                    convert
+                        .click_convertLead()
+                }
+                    else
+                    {
+                        browser
+                            .refresh()
+                        convert
+                            .click_convertLead()
+                    }
+            })
     },
 
     'THEN I shall be able to view the Convert Lead page in a new tab': function (browser) 
