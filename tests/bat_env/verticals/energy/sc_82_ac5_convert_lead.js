8d0bb0272f5c0a43f70bbcd16078dff94ddc2557
roger.cabatic <roger.cabatic@iselect.com.au>
2019-11-07 11:28:06 +0800

    no message

--- tests/bat_env/verticals/energy/sc_82_ac5_convert_lead.js
+++ tests/bat_env/verticals/energy/sc_82_ac5_convert_lead.js
@@ -9,15 +9,17 @@ module.exports = {
     },
     '(AC-1) GIVEN that the results page is displayed': function(browser) 
     {
-        utils.global_search(browser,'Howie Newbie');
+        utils.global_search(browser,'Ger Ger');
         browser.saveScreenshot('reports/bat_env/verticals/energy/energytelco_consultant/a_person_accountAndopportunity_tests/sp_650_global_search/ac1_display_the_recent_items.png')        
     },
     '(AC-1) WHEN I select any one of the records on the results page that has a hyperlink': function (browser) 
     {
-        utils.global_search_select_data(browser,  'Leads', 'Howie Newbie', 'https://iselect--bat.lightning.force.com/', null, 'resultsItem');
+        utils.global_search_select_data(browser,  'Leads', 'Ger Ger Lead', 'https://iselect--bat.lightning.force.com/', null, 'resultsItem');
     },
     '(AC 4- Press convert lead button)'(browser){
+        utils.save_ScreenShot(browser,'reports/bat_env/verticals/energy/ss_beforeClickConvertLead.png');
         convert.click_convertLead();
+        utils.save_ScreenShot(browser,'reports/bat_env/verticals/energy/ss_afterClickConvertLead.png');
     },
     '(AC5: Ensure correct details are display in Convert Lead page)'(browser){
         utils.validation_convert_lead_page(browser,leadConsultantlayout.Salesforce.Lead.convertLeadFields);
